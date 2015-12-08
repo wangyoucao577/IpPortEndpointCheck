b27234522938196bfc04893183d4414fc2540789
wangyoucao577@gmail.com <wangyoucao577@gmail.com>
2015-12-08 18:43:40 +0800

    添加UDPServer类，准备实习UDP Server相关功能。

---
+++ IpPortEndpointCheck/UdpServer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IpPortEndpointCheck
+{
+    class UdpServer : NetCheckServers
+    {
+        public override void StartListen()
+        {
+            base.StartListen();
+
+            DoStart(UdpServer.UdpReceiveThreadProc);
+        }
+
+        static private void UdpReceiveThreadProc(object data)
+        {
+            //TODO: add udp function here
+
+            //TcpListeners tcpListener = (TcpListeners)data;
+            //int port = tcpListener.PopPort();
+
+
+            //TcpListener listener = new TcpListener(IPAddress.Any, port);
+
+            //try
+            //{
+            //    listener.Start();
+
+            //    while (tcpListener.GoonListen)
+            //    {
+            //        TcpClient cli = listener.AcceptTcpClient();
+            //        cli.Close();
+            //    }
+
+            //    listener.Stop();
+            //}
+            //catch (SocketException ex)
+            //{
+            //    switch (ex.ErrorCode)
+            //    {
+            //        case 10048:
+            //            break;
+            //        default:
+            //            Debug.Assert(false);
+            //            break;
+            //    }
+            //}
+
+            //tcpListener.DoDecrease();
+        }
+    }
+}
