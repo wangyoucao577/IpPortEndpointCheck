a1a85ee0cc361df4dfc7dac95b6107d341eec4ff
wangyoucao577 <wangyoucao577@gmail.com>
2016-08-27 14:13:56 +0800

    代码中操作更新标题栏，防止每次UI模式编辑时标题自动变掉的问题。

--- IpPortEndpointCheck/MainForm.cs
+++ IpPortEndpointCheck/MainForm.cs
@@ -19,6 +19,7 @@ namespace IpPortEndpointCheck
         public MainForm()
         {
             InitializeComponent();
+            this.Text = ProductName + "  " + ProductVersion;
         }
 
         private void button1_Click(object sender, EventArgs e)
