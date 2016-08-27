c01f6e3f6ba56b24a13473454be177134f8a8214
wangyoucao577 <wangyoucao577@gmail.com>
2016-08-27 14:10:01 +0800

    修正标题栏的显示。

--- IpPortEndpointCheck/MainForm.Designer.cs
+++ IpPortEndpointCheck/MainForm.Designer.cs
@@ -316,7 +316,7 @@
             this.MinimumSize = new System.Drawing.Size(576, 420);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Find Open Port  4.6.1038.0";
+            this.Text = ProductName + "  " + ProductVersion;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.groupBox1.ResumeLayout(false);
