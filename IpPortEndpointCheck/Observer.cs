a6fd63f28b21c97ca221c02f31016aaf3d097076
wangyoucao577 <wangyoucao577@gmail.com>
2016-08-31 16:47:41 +0800

    新增之前写的简单的Observer两个文件，准备用于把内部的关键log通过notify的方式反馈给GUI，从而用于显示。

---
+++ IpPortEndpointCheck/Observer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Observer
+{
+    interface IObserver
+    {
+       void Update(object sub);
+    }
+}
---
+++ IpPortEndpointCheck/Subject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Observer
+{
+    class Subject
+    {
+        protected List<IObserver> m_objectList = new List<IObserver>();
+
+        public void Attach(IObserver obj)
+        {
+            m_objectList.Add(obj);
+        }
+
+        public void Detach(IObserver obj)
+        {
+            m_objectList.Remove(obj);
+        }
+
+        public void DetachAll()
+        {
+            m_objectList.Clear();
+        }
+
+        public virtual void Notify()
+        {
+            foreach (IObserver item in m_objectList)
+            {
+                item.Update(this);
+            }
+        }
+    }
+}
